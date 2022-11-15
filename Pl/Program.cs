39093c554da2b09af4c3ff6eef9f0bb8035073fa
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-16 00:50:45 +0100

    Checking file connection

--- Pl/Program.cs
+++ Pl/Program.cs
@@ -1,5 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Linq.Expressions;
+﻿using System;
+using System.IO;
+using System.Text;
+string link = @"accounts.txt";
+if (File.Exists(link)) {
+    Console.WriteLine("Plik istnieje");
+} else
+{
+    
+    using (FileStream fs = File.Create(link))
+    {
+
+    }
+}
+
 restart:
 Console.WriteLine("Witaj drogi kliencie, czy posiadasz już konto w naszym banku?");
 begining:
@@ -13,10 +26,10 @@ if (ifAcc == "1")
     Console.Clear();
     Console.WriteLine("Witamy w systemie logowania naszego banku");
     Console.WriteLine("Podaj Numer konta:");
-    String login = Console.ReadLine();
+    string login = Console.ReadLine();
     Console.WriteLine($"Podaj hasło");
 haslo:
-    String psswd = Console.ReadLine();
+    string psswd = Console.ReadLine();
     if (psswd == "17772")
     {
         Console.WriteLine("Hasło się zgadza");
@@ -25,18 +38,19 @@ haslo:
     {
         Console.WriteLine("hasło niepoprawne, spróbuj ponownie");
         goto haslo;
-        
+
     }
     bool loggedIn = true;
 }
-else if(ifAcc == "2")
+else if (ifAcc == "2")
 {
+accDataRepair:
     Console.Clear();
     Console.WriteLine("Witamy w kreatorze tworzenia konta, Podaj dane osobiste o które zostaniesz poproszony/a");
     Console.Write("Imię:");
-    rename:
+rename:
     string newAccName = Console.ReadLine();
-    if(String.IsNullOrEmpty(newAccName))
+    if (string.IsNullOrEmpty(newAccName))
     {
         Console.WriteLine("Zakładka imię nie może być pusta, spróbuj ponownie");
         Console.WriteLine("Naciśnij enter by kontynuować");
@@ -46,9 +60,9 @@ else if(ifAcc == "2")
         goto rename;
     }
     Console.Write("Nazwisko:");
-    resurname:
+resurname:
     string newAccSurname = Console.ReadLine();
-    if (String.IsNullOrEmpty(newAccSurname))
+    if (string.IsNullOrEmpty(newAccSurname))
     {
         Console.WriteLine("Zakładka nazwisko nie może być pusta, spróbuj ponownie");
         Console.WriteLine("Naciśnij enter by kontynuować");
@@ -57,11 +71,11 @@ else if(ifAcc == "2")
         Console.Write("Proszę ponownie podać nazwisko:");
         goto resurname;
     }
-    Console.Write("(Data urodzenia powinna być w formacie DD-MM-RRRR)");
-    Console.WriteLine("Data Urodzenia:");
-    rebirthdate:
+    Console.WriteLine("(Data urodzenia powinna być w formacie DD-MM-RRRR)");
+    Console.Write("Data Urodzenia:");
+rebirthdate:
     string newAccBirthdate = Console.ReadLine();
-    if (String.IsNullOrEmpty(newAccBirthdate))
+    if (string.IsNullOrEmpty(newAccBirthdate))
     {
         Console.WriteLine("Zakładka Data urodzenia nie może być pusta, spróbuj ponownie");
         Console.WriteLine("Naciśnij enter by kontynuować");
@@ -71,29 +85,63 @@ else if(ifAcc == "2")
         Console.Write("Proszę ponownie podać Datę urodzenia:");
         goto rebirthdate;
     }
+notSure:
     Console.Clear();
     Console.WriteLine("Twoje dane osobowe to:");
-    Console.WriteLine($"{newAccName},{newAccSurname}{newAccBirthdate}");
+    Console.WriteLine($"{newAccName}, {newAccSurname}, urodzona dnia {newAccBirthdate}");
     Console.WriteLine("Czy dane są poprawne?");
     Console.WriteLine("1. Tak");
     Console.WriteLine("2. Nie");
     string confirmation = Console.ReadLine();
-    if(confirmation == "1") 
+
+    if (confirmation == "1" || confirmation.ToLower() == "tak")
     {
-        Console.WriteLine("Dodawanie danych do bazy");
+
     }
-    else
+    else if (confirmation == "2" || confirmation.ToLower() == "nie")
     {
         Console.WriteLine("Czy na pewno chcesz poprawić informacje dotyczące konta? (Czynności nie można cofnąć)");
-        Console.WriteLine("1. Tak");
-        Console.WriteLine("2. NIe");
+        Console.WriteLine("Żeby potwierdzić wpisz TAK");
         confirmation = Console.ReadLine();
-        if()
-    Console.Clear();
+        if (confirmation == "TAK")
+        {
+            Console.Clear();
+            goto accDataRepair;
+        }
+        else
+        {
+            goto notSure;
+        }
 
-    goto restart;
+    }
+}
+else if (ifAcc == "0")
+{
+    Console.WriteLine("Dziękujemy za skorzystanie z aplikacji naszego banku i życzymy miłego dnia");
+    Console.WriteLine("By zamknąc naciśnij enter");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Niepoprawna odpowiedź");
+    Console.Clear();
+    goto begining;
 }
 
 
 
 
+
+/*zapisz kod
+ * try
+{
+    using (FileStream fs = File.Create(link))
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes("dane testowe");
+        fs.Write(info,0,info.Length);
+    }
+}catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+*/
